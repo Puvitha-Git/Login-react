ce489ac13c24bd170dfe0c4cc2c11836fb78f521
Puvitha-Git <puvithaganapathi@gmail.com>
2025-06-23 20:37:12 +0530

    firat commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginSignUp from "./components/Assets/LoginSignUp/LoginSignUp.jsx";
+import LoginSignUp from "./components/Assets/LoginSignUp/LoginSignUp";
 
 import "./App.css";
 
